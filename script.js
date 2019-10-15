a61b1f5a06bc005357dead48fccf0d5bc3b9a6e7
Kaio <kaiofelipe.js@gmail.com>
2019-10-15 11:50:38 -0300

    refactored functions for improvement reading

--- script.js
+++ script.js
@@ -4,26 +4,40 @@ const listItem = document.querySelector('#input-list');
 function addUpdateEvent(btn, element) {
   btn.addEventListener('click', () => {
     if (element.classList.contains('removed')) {
-      element.classList.remove('removed')
-      btn.innerHTML = '❌'
+      element.classList.remove('removed');
+      btn.innerHTML = '❌';
     } else {
-      element.classList.add('removed')
-      btn.innerHTML = '✅'
+      element.classList.add('removed');
+      btn.innerHTML = '✅';
     }
   });
 }
 
+function createDivElement() {
+  return document.createElement('div');
+}
+
+function createSpanElement() {
+  return document.createElement('span');
+}
+
+function createButtonElement() {
+  return document.createElement('button');
+}
+
 function createToDoItem() {
-  let divItem = document.createElement('div');
-  divItem.classList.add('item')
-  let spanItem = document.createElement('span');
+  let divItem = createDivElement();
+  divItem.classList.add('item');
+
+  let spanItem = createSpanElement();
   spanItem.innerText = textInput.value;
   divItem.appendChild(spanItem);
+
   listItem.appendChild(divItem);
 
-  let btnRemove = document.createElement('button');
+  let btnRemove = createButtonElement();
   btnRemove.innerHTML = '❌';
-  btnRemove.classList.add('btn-remove')
+  btnRemove.classList.add('btn-remove');
   divItem.appendChild(btnRemove);
 
   addUpdateEvent(btnRemove, divItem);
